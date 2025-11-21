171af81d9b1ec5c17af07f030157f8dffc5997f8
Alvaroalonsobabbel <aalonso@babbel.com>
2025-11-21 10:12:57 +0100

    Add initial Postgres migrations and update SQL for pgx/sqlc

---
+++ db/migrations/000001_add_initial_tables.up.sql
@@ -0,0 +1,34 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS queries (
+    id BIGSERIAL PRIMARY KEY,
+    keywords TEXT NOT NULL,
+    location TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    queried_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP, -- Last time RSS client used the query.
+    updated_at TIMESTAMPTZ, -- Last time the query was executed.
+    UNIQUE (keywords, location)
+);
+
+CREATE INDEX IF NOT EXISTS idx_queries_keywords_location ON queries (keywords, location);
+
+CREATE TABLE IF NOT EXISTS offers (
+    id TEXT PRIMARY KEY,
+    title TEXT NOT NULL,
+    company TEXT NOT NULL,
+    location TEXT NOT NULL,
+    posted_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX IF NOT EXISTS idx_offers_posted_at ON offers (posted_at DESC);
+
+CREATE TABLE IF NOT EXISTS query_offers (
+    query_id BIGINT NOT NULL,
+    offer_id TEXT NOT NULL,
+    PRIMARY KEY (query_id, offer_id),
+    FOREIGN KEY (query_id) REFERENCES queries (id) ON DELETE CASCADE,
+    FOREIGN KEY (offer_id) REFERENCES offers (id) ON DELETE CASCADE
+);
+
+COMMIT;
